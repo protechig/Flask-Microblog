b0209039c9ca17b6aee5aa8fcd0c3c5199851b94
Zach Russell <zach@protechig.com>
2016-01-11 04:02:32 -0500

    microblog part 2

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'nickname': 'Zach'}
+    posts = [ # fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+            },
+        {
+            'author': {'nickname': 'Pablo' },
+            'body': 'Luke, I am your father!'
+            }
+        ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
